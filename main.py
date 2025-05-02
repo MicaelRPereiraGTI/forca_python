5beebe8065eeb975ab129e28d9c7c99578a7cb5d
whoami micael <62766013+MicaelRPereiraGTI@users.noreply.github.com>
2025-05-02 13:56:33 +0000

    forca python

---
+++ main.py
@@ -0,0 +1,37 @@
+import random
+
+palavras = ['python', 'programacao', 'computador', 'aula', 'variavel']
+palavra = random.choice(palavras)
+letras_usuario = []
+chances = 7
+ganhou = False
+
+while True:
+  #criar a logica 
+  for letra in palavra:
+    if letra.lower() in letras_usuario:
+      print(letra, end=" ")
+    else:
+      print("_", end=" ")
+  print(f"Você tem {chances} chances")
+
+  tentativa = input("Escolha uma letra para advinhar: ")
+  letras_usuario.append(tentativa.lower())
+  if tentativa.lower() not in palavra.lower():
+    chances -= 1
+ 
+
+  ganhou = True
+  for letra in palavra:
+    if letra.lower() not in letras_usuario:
+      ganhou = False
+
+
+  if chances == 0 or ganhou:
+    break
+
+
+if ganhou:
+  print(f"Parabéns. você ganhou. A palavra era: {palavra}")
+else:
+  print(f"Você perdeu. A palavra era: {palavra}")
